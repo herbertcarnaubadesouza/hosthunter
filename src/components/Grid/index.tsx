efb54486a83b83a2988e8906dd2729798630cb19
Herbert Carnauba de Souza <80016718+herbertcarnaubadesouza@users.noreply.github.com>
2022-10-17 10:44:34 -0300

    ajusting build

--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -22,7 +22,7 @@ export function Grid() {
                 <div className={styles.contentText}>
                   <p>
                     is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy
+                    industry. Lorem Ipsum has been the industrys standard dummy
                     text ever since the 1500s
                   </p>
                   <p className={styles.clientName}>Jeff Colinn</p>
@@ -51,7 +51,7 @@ export function Grid() {
                 <div className={styles.contentText}>
                   <p>
                     is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy
+                    industry. Lorem Ipsum has been the industrys standard dummy
                     text ever since the 1500s
                   </p>
                   <p className={styles.clientName}>Jeff Colinn</p>
@@ -93,7 +93,7 @@ export function Grid() {
                 <div className={styles.contentText}>
                   <p>
                     is simply dummy text of the printing and typesetting
-                    industry. Lorem Ipsum has been the industry's standard dummy
+                    industry. Lorem Ipsum has been the industrys standard dummy
                     text ever since the 1500s
                   </p>
                   <p className={styles.clientName}>Jeff Colinn</p>
